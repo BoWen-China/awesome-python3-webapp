5583ebc6396d875815508f77498c89e9c14de09e
BoWen <cbwwork@outlook.com>
2019-04-01 15:54:40 +0800

    add annotation and update web.application argument

--- www/app.py
+++ www/app.py
@@ -18,14 +18,18 @@ def index(request):
 
 @asyncio.coroutine
 def init(loop):
-	app = web.Application(loop=loop)
+	# 循环参数已弃用
+	app = web.Application()
 	# 填加路由节点 
 	# 访问方式为get 路径地址为/ 用index 方法响应
 	app.router.add_route('GET','/',index)
+	# make_handler 已弃用
 	srv = yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
 	logging.info('server started at http://127.0.0.1:9000...')
 	return srv
-
+# 异步消息循环
 loop = asyncio.get_event_loop()
+# 直到运行完成运行的时间
 loop.run_until_complete(init(loop))
+# 运行到stop()调用
 loop.run_forever()
